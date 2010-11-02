00560600739cc85769749d1d47b9f6cb900e1676
Adrian Moya <adrianmoya@gmail.com>
2010-11-02 19:43:19 +0000

    Updating the is_running method

--- patchtool/utils.py
+++ patchtool/utils.py
@@ -1,8 +1,9 @@
 from subprocess import Popen, PIPE
 
 def is_running(process):
-   processes = Popen(['ps', 'xa'], stdout=PIPE).communicate()[0]
-   if processes.find(process) > 0:
+   processes = Popen(['ps', 'xa', '-o','args'], stdout=PIPE)
+   running = Popen(['grep',process], stdin=processes.stdout, stdout=PIPE)
+   if running.find(process) > 0:
       return True
    return False
 
