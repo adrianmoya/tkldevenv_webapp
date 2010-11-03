977d05af80cf93fd51459a0a4ff3a5355c4e35cd
Adrian Moya <adrianmoya@gmail.com>
2010-11-03 22:32:39 +0000

    Index page listing projects and starting the menu

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from tkldevenv.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
     (r'^patchtool/', include('tkldevenv.patchtool.urls')),
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_DOC_ROOT}),
+    (r'^$', main)
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
